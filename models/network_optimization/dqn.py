9d9b17dc47bc67753d8a2d576f3523155172380d
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-30 00:41:38 -0700

    Create dqn.py

---
+++ models/network_optimization/dqn.py
@@ -0,0 +1,86 @@
+import numpy as np
+import random
+import tensorflow as tf
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95  # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+        self.target_model = self._build_model()
+        self.update_target_model()
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.target_model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+if __name__ == "__main__":
+    env = NetworkEnvironment()  # Assume NetworkEnvironment is defined elsewhere
+    state_size = env.state_size
+    action_size = env.action_size
+    agent = DQNAgent(state_size, action_size)
+    done = False
+    batch_size = 32
+    EPISODES = 1000
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                agent.update_target_model()
+                print(f"episode: {e}/{EPISODES}, score: {time}, e: {agent.epsilon:.2}")
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+        if e % 10 == 0:
+            agent.save(f"models/network_optimization/dqn_{e}.h5")
